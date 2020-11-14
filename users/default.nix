6931fdf9c10f36050dcf1a58f6140a4ac4eb248f
jules-goose <jules.goos3@gmail.com>
2020-11-14 15:51:33 +0000

    should work

--- users/default.nix
+++ users/default.nix
@@ -1,4 +1,4 @@
 self:
 {
-    matthew = import ./jules self; # pass 'self' in order to allow ./users/default.nix -> ./users/jules/default.nix to access ${self}, to provide a path relative to flake.nix.
+    jules = import ./jules self; # pass 'self' in order to allow ./users/default.nix -> ./users/jules/default.nix to access ${self}, to provide a path relative to flake.nix.
 }
--- users/jules/default.nix
+++ users/jules/default.nix
@@ -5,7 +5,6 @@ self:
 {
 imports =
     [
-        ./modules
     ];
 
 home.username = "jules";
