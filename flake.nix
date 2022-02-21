80ada121ba32c8f21217868ee4fac33d1748065d
jules-goose <59726279+jules-goose@users.noreply.github.com>
2022-02-21 16:25:38 +0100

    added shinonome to flake.nix

--- flake.nix
+++ flake.nix
@@ -23,6 +23,20 @@
         ];
         specialArgs = { inherit inputs; };
       };
+      shinonome = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (import ./hosts/shinonome/configuration.nix)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users = import ./users self; # pass 'self' in order to allow ./users/default.nix -> ./users/matthew/default.nix to access ${self}, to provide a path relative to flake.nix.
+          }
+          { nixpkgs.overlays = [ nur.overlay ]; }
+        ];
+        specialArgs = { inherit inputs; };
+      };
     };
   };
 }
