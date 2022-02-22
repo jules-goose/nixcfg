96e2d25bec6b7bafe82f740a639f1014f41e7eb1
jules-goose <59726279+jules-goose@users.noreply.github.com>
2022-02-22 09:54:40 +0100

    zzzzzz

--- flake.nix
+++ flake.nix
@@ -27,12 +27,12 @@
         system = "x86_64-linux";
         modules = [
           (import ./hosts/shinonome/configuration.nix)
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users = import ./users self; # pass 'self' in order to allow ./users/default.nix -> ./users/matthew/default.nix to access ${self}, to provide a path relative to flake.nix.
-          }
+#          home-manager.nixosModules.home-manager
+#          {
+#            home-manager.useGlobalPkgs = true;
+#            home-manager.useUserPackages = true;
+#            home-manager.users = import ./users self; # pass 'self' in order to allow ./users/default.nix -> ./users/matthew/default.nix to access ${self}, to provide a path relative to flake.nix.
+#          }
           { nixpkgs.overlays = [ nur.overlay ]; }
         ];
         specialArgs = { inherit inputs; };
