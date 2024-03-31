242c2e8fd3874a269601656ed7c9715412bdfe7f
jules-goose <jules.michel475@gmail.com>
2024-03-31 19:16:39 +0200

    update to flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs2305.url = "github:nixos/nixpkgs/nixos-23.05";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
+      
     };
     };
 
-  outputs = { self, home-manager, nixpkgs, ... }@inputs: {
+  outputs = 
+  { 
+    self
+    , home-manager
+    , nixpkgs
+    , nixpkgs2305
+    , ... 
+    } @ inputs: {
     nixosConfigurations = {
       mandrake = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
